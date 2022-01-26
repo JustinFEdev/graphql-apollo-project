779882ac9def1a47bfa07026851f45c4ba25f6d3
Justin_supertree <justin1688@supertree.co>
2022-01-26 16:01:25 +0900

    add client side folder

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Here is Client Side</h1>
+    </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,68 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
-const userData = require("./MOCK_DATA.json");
-const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList } = graphql;
+const PORT = 1688;
 const { graphqlHTTP } = require("express-graphql");
-
-const UserType = new GraphQLObjectType({
-  name: "User",
-
-  fields: () => ({
-    id: { type: GraphQLInt },
-    firstName: { type: GraphQLString },
-    lastName: { type: GraphQLString },
-    email: { type: GraphQLString },
-    password: { type: GraphQLString },
-  }),
-});
-
-const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  fields: {
-    getAllUsers: {
-      type: new GraphQLList(UserType),
-      args: { id: { type: GraphQLInt } },
-      resolve(parent, args) {
-        return userData;
-      },
-    },
-  },
-});
-
-const Mutation = new GraphQLObjectType({
-  name: "Mutation",
-  fields: {
-    createUser: {
-      type: UserType,
-      args: {
-        firstName: { type: GraphQLString },
-        lastName: { type: GraphQLString },
-        email: { type: GraphQLString },
-        password: { type: GraphQLString },
-      },
-      resolve(parent, args) {
-        userData.push({
-          id: userData.length + 1,
-          firstName: args.firstName,
-          lastName: args.lastName,
-          email: args.email,
-          password: args.password,
-        });
-        return args;
-      },
-    },
-  },
-});
-
-const schema = new GraphQLSchema({ query: RootQuery, mutations: Mutation });
+const schema = require("./Schemas");
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    graphql: true,
+    graphiql: true,
   })
 );
 
